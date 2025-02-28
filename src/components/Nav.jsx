ecf4960837c3c953d9ec4f4a7468b93e517f70d6
MilenaGarciaCosta <milenagarcia.dev@gmail.com>
2025-02-28 19:03:25 -0300

    tela de formulário estilizada

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,14 +7,20 @@ const Nav = () => {
         <>
             <nav className="bg-white dark:bg-sky-950 w-full z-20 top-0 start-0 border-b border-gray-200">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                    <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <img src={main_icon} className="h-8 main-icon" alt="Logo" />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">InstaCoders</span>
-                    </a>
-                    <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                    <Link to='/' className="flex items-center space-x-3 rtl:space-x-reverse">
+                        <img src={main_icon} id='main-icon' className="h-8" alt="Logo" />
+                        <span id='main-title' className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">InstaCoders</span>
+                    </Link>
+                    <div id='nav-button-group' className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse items-center gap-5">
                         <Link to='/cadastrar'>
-                            <button class="btn-entrar bg-sky-950 text-cyan-100 font-medium rounded-full cursor-pointer px-10 py-4 text-lg border-1 border-sky-300 transition-all ease-in-out duration-200">Entrar</button>
+                            <button className="btn-entrar bg-sky-950 text-cyan-100 font-medium rounded-full cursor-pointer px-10 py-4 text-lg border-1 border-sky-300 transition-all ease-in-out duration-200">Entrar</button>
                         </Link>
+                        <label className="mode-switch">
+                            <input type="checkbox" />
+                            <div className="slider">
+                                <div className="circle"></div>
+                            </div>
+                        </label>
                     </div>
                 </div>
             </nav>
--- src/routers/Formulario.jsx
+++ src/routers/Formulario.jsx
@@ -1,9 +1,53 @@
 import '../styles/formulario.css'
 
-const Formulario =()=>{
-    return(
-        <section id="main">
-            <h1>Formulário</h1>
+const Formulario = () => {
+    return (
+        <section id="main" className='flex-col'>
+            <h1 className='page_title'>Crie sua conta!</h1>
+
+            <section className='formulario_cadastro' data-formulario>
+                <div>
+                    <fieldset className="formulario_campo">
+                        <label className="campo_etiqueta" for="nome">Nome</label>
+                        <input name="nome" id="nome" className="campo_escrita" />
+                        <span className="mensagem-erro"></span>
+                    </fieldset>
+
+                    <fieldset className="formulario_campo">
+                        <label className="campo_etiqueta" for="email">E-mail</label>
+                        <input name="email" id="email" className="campo_escrita" />
+                        <span className="mensagem-erro"></span>
+                    </fieldset>
+                    <fieldset className="formulario_campo">
+                        <label className="campo_etiqueta" for="cpf">CPF (apenas números)*</label>
+                        <input name="cpf" id="cpf" className="campo_escrita campo_escrita--menor" required />
+                        <span className="mensagem-erro"></span>
+                    </fieldset>
+                    <fieldset className="formulario_campo">
+                        <label className="campo_etiqueta" for="aniversario">Data de nascimento</label>
+                        <input name="aniversario" id="aniversario" className="campo_escrita campo_escrita--menor" />
+                        <span className="mensagem-erro"></span>
+                    </fieldset>
+                    <fieldset className="formulario_termos">
+                        <input type='checkbox' name="termos" className="termos_input" />
+                        <p className="termos_texto">Li e estou ciente quanto às condições de tratamento dos meus dados conforme
+                            descrito na PolíticadePrivacidade do banco.</p>
+                        <span className="mensagem-erro"></span>
+                    </fieldset>
+                </div>
+
+                <div>
+                        <button class="btn-avancar">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"></path>
+                            </svg>
+                            <div class="text">
+                                <input type='button' value="Avançar" className="formulario_botao" id="enviar" data-botao-enviar />
+                            </div>
+                        </button>
+                    <span className="mensagem-erro"></span>
+                </div>
+            </section>
         </section>
     )
 }
